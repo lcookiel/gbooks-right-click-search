b5af1fa024e692e7a625ecd72ee16e3c9c6fa7b9
eleazarlevchenko <eleazar.levchenko@gmail.com>
2021-05-01 10:51:38 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+function doSearch (search_target, tab)
+
+{
+	
+   chrome.tabs.create( 
+      {
+
+	url : "https://www.google.com/search?tbm=bks&q="+search_target,
+ active : false,
+ index : tab.index + 1
+
+      } );
+
+}
+
+
+
+function selectionHandler (info, tab)
+
+{
+
+   var replaced = info.selectionText.replace(/ /g, "+");
+   doSearch( replaced, tab );
+
+}
+
+
+
+function resetContextMenus ()
+
+{
+
+   chrome.contextMenus.removeAll(
+ function()
+
+	{
+	   var id = chrome.contextMenus.create( 
+		{
+		   
+title: "Search Google Books for '%s'",
+ contexts: [ "selection" ], onclick: selectionHandler
+ 
+		} ); 
+	}
+ );
+
+}
+
+
+
+resetContextMenus();
